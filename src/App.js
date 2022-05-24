b405f11a575a46fbc6a41729c5615bab1a5ebefa
ravi2018cs115abesit@gmail.com <ravi2018cs115abesit@gmail.com>
2022-05-24 12:14:28 +0530

    project complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Routes, Route } from "react-router-dom"
+import AppBar from './components/ResponsiveAppBar'
+import AddShop from './Pages/AddShop'
+import ViewShop from './Pages/ViewShop'
+import UpdateShop from './Pages/UpdateShop'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar/>
+    <Routes>
+        <Route path="/" element={ <AddShop/> } />
+        <Route path="/View" element={ <ViewShop/> } />
+        <Route path="/Update" element={ <UpdateShop/> } />
+
+      </Routes>
+    </div >
   );
 }
 
